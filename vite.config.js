f1d000a1d9d0ab360da5ba6e3b61d25e726df78d
asdzxcvbn1509a <asdzxcvbn1509a@gmail.com>
2025-09-13 23:46:37 +0700

    deploy5

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        }
+      }
+    }
+  },
   server: {
     host: true, // อนุญาตให้เข้าถึงจากภายนอก
     port: 5173, // หรือ port ที่คุณใช้
